bd9a89885b919a05f4de0a31ceb987f4bbbdcb92
XinshenXiangChen <xinshenxiangchen@gmail.com>
2025-12-12 20:13:14 +0100

    users

--- UserService/UserServiceConfig/urls.py
+++ UserService/UserServiceConfig/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('user/', include('user_app.urls')),
+    path('users/', include('user_app.urls')),
 ]
